0a76c177dd14d5e0413b43b905e31bc7def58c46
koyu <me@koyu.space>
2021-07-24 12:27:29 +0200

    Fix nowplaying

--- static/app.js
+++ static/app.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
     function updateNowPlaying() {
         $.getJSON("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=bubblineyuri&api_key=1ebf7f35cb5aa1cb9b9804f88fb16339&format=json&limit=1", function(data) {
-            $("#nowplaying").html("<i class=\"fa fa-play\" aria-hidden=\"true\"></i> "+data["recenttracks"]["track"][0]["artist"]["#text"]+" – "+data["recenttracks"]["track"][0]["name"]);
+            try {
+                if (data["recenttracks"]["track"][0]["@attr"]["nowplaying"] === "true") {
+                    $("#nowplaying").html("<i class=\"fa fa-play\" aria-hidden=\"true\"></i> "+data["recenttracks"]["track"][0]["artist"]["#text"]+" – "+data["recenttracks"]["track"][0]["name"]);
+                } else {
+                    $("#nowplaying").html("<i class=\"fa fa-pause\" aria-hidden=\"true\"></i> "+data["recenttracks"]["track"][0]["artist"]["#text"]+" – "+data["recenttracks"]["track"][0]["name"]);
+                }
+            } catch (e) {
+                $("#nowplaying .fa").attr("class", "fa fa-pause");
+                if (!$("#nowplaying").html().includes(" ")) {
+                    $("#nowplaying").html("<i class=\"fa fa-pause\" aria-hidden=\"true\"></i> "+data["recenttracks"]["track"][0]["artist"]["#text"]+" – "+data["recenttracks"]["track"][0]["name"]);
+                }
+            }
         });
     }
     $("body").html(twemoji.parse($("body").html(), {folder: "svg", ext: ".svg"}));
@@ -9,6 +20,6 @@ $(document).ready(function() {
     updateNowPlaying();
     window.setInterval(function() {
         updateNowPlaying();
-    }, 1000);
+    }, 500);
     $("body").html($("body").html().replaceAll("href=\"https://", "target=\"_blank\" href=\"https://"));
 });
