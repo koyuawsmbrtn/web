0694fdb4e88f6b9eb302386eb1986dbc44c57541
koyu <me@koyu.space>
2022-04-03 13:50:12 +0200

    Add Discord banner

--- static/app.js
+++ static/app.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
     $("body").html(twemoji.parse($("body").html(), {folder: "svg", ext: ".svg"}));
+    $("#discord").html("<img src=\"https://lanyard-profile-readme.vercel.app/api/635125063896793098?bg=000&date="+new Date().getTime()+"\">")
+    window.setInterval(function() {
+        $("#discord img").attr("src", "https://lanyard-profile-readme.vercel.app/api/635125063896793098?bg=000&date="+new Date().getTime());
+    }, 500);
 });
