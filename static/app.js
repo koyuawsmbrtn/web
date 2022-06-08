fcfbf2b5b3e00dc2a7bcf3955256b665c8a0e180
koyu <me@koyu.space>
2022-06-08 15:20:47 +0200

    Add Twemoji to Footer

--- static/app.js
+++ static/app.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
     $("main").html(twemoji.parse($("main").html(), {folder: "svg", ext: ".svg"}));
+    $("footer").html(twemoji.parse($("footer").html(), {folder: "svg", ext: ".svg"}));
 });
