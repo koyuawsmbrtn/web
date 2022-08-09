634d94f514cf5d174f690b56a699ff3a9521e639
koyu <me@koyu.space>
2022-08-09 23:42:37 +0200

    Strip down site a little more

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
+	vite: {
+		ssr: {
+			external: ["svgo"],
+		},
+	},
 	adapter: vercel(),
 	output: "server"
 });
