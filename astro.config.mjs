5929bbf8c5f81ee2dc4c53a9861ce5445ebbfa56
koyu <me@koyu.space>
2022-12-17 15:52:44 +0100

    Remove redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
-// import turbolinks from'@astrojs/turbolinks';
+import turbolinks from'@astrojs/turbolinks';
 
 export default defineConfig({
-  // integrations: [turbolinks()],
-  output: 'server',
-  adapter: netlify()
+  integrations: [turbolinks()],
 });
