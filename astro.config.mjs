c1638f1c2936bde112125b45ce4ef787875bb0c0
koyu <me@koyu.space>
2023-02-10 05:37:48 +0100

    Remove Turbolinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import turbolinks from'@astrojs/turbolinks';
 
 export default defineConfig({
-  integrations: [turbolinks()],
   site: 'https://web.koyu.space',
 });
