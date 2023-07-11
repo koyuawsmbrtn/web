f999fd94620bc23d7998f0803d7cc61283632f1f
koyu <me@koyu.space>
2023-07-11 11:58:11 +0200

    Update

--- tasks/clean.js
+++ tasks/clean.js
@@ -13,6 +13,8 @@ const removeSub = (s) => {
     return d;
 }
 
+console.log("Cleaning up logos...");
+
 fs.readFile("src/json/instances.json", 'utf8', (err, data) => {
     if (err) {
         console.log(err);
@@ -23,7 +25,10 @@ fs.readFile("src/json/instances.json", 'utf8', (err, data) => {
         var logo = instance.logo;
         if (logo.startsWith("http://") || logo.startsWith("https://")) {
             var extension = logo.split("/")[logo.split("/").length - 1].split(".")[1] == undefined || logo.split("/")[logo.split("/").length - 1].split(".")[1].startsWith("com") ? "png" : logo.split("/")[logo.split("/").length - 1].split(".")[1];
-            fs.rm(`public/fedilist/${removeSub(instance.instance).replace(".", "")}.${extension}`, () => {});
+            var filename = `public/fedilist/${removeSub(instance.instance).replace(".", "")}.${extension}`;
+            fs.rm(filename, () => {
+                console.log(`Deleted ${filename}`);
+            });
         }
     });
 });
