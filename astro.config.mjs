2fa424bfee008e3d7d8b57b02c6ead6e911ef77d
koyu <me@koyu.space>
2024-01-03 12:27:27 +0100

    Update to astro-icon v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://web.koyu.space',
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    icon({
+      include: {
+        "simple-icons": ["*"],
+        "fa-solid": ["envelope"]
+      }
+    })
+  ],
   prefetch: {
     prefetchAll: true
   }
