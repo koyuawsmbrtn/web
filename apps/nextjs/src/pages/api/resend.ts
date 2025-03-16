bf9a91b8d4d0e0598bf7f4b535f716ca9ceb5c9e
koyu <me@koyu.space>
2025-03-16 20:36:52 +0100

    feat: enhance contact form email notifications with detailed submission data

--- apps/nextjs/src/pages/api/resend.ts
+++ apps/nextjs/src/pages/api/resend.ts
@@ -11,11 +11,26 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<ResponseData>
 ) {
+    const template = {
+        subject: 'New contact form submission',
+        html: `
+            <h1>New contact form submission</h1>
+            <p>Someone has submitted the contact form on your website.</p>
+            <h2>Details</h2>
+            <p>Submitted at: ${new Date().toISOString()}</p>
+            <h2>Submission</h2>
+            <p>IP: ${req.headers['x-real-ip'] || req.socket.remoteAddress}</p>
+            <p>User agent: ${req.headers['user-agent']}</p>
+            <h2>Form data</h2>
+            <p>Name: ${req.body.name}</p>
+            <p>Email: ${req.body.email}</p>
+            <p>Message:<br>${req.body.message}</p>
+        `,
+    };
     await resend.emails.send({
         from: 'koyu\'s personal website <no-reply@notifications.koyu.space>',
         to: 'me@koyu.space',
-        subject: 'New contact form submission',
-        text: req.body.message,
+        ...template,
     });
-    res.status(200).json({ message: 'Email sent' });
+    res.status(302).setHeader('Location', '/contact-success').end();
 }
