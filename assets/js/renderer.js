b9e41053492cec48eb0dd71bc45dcbe1bc1fe6e7
koyu <me@koyu.space>
2025-06-24 03:53:09 +0200

    Add contact page with social links and SCM Music Player integration

--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -9,5 +9,12 @@ fetch("/parts/footer.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("render-footer").innerHTML = data;
+
+    // Transing the internet!
+    const container = document.getElementById("buttons");
+    const script1 = document.createElement("script");
+    script1.src = "https://transring.neocities.org/ring.js";
+    script1.type = "text/javascript";
+    container.appendChild(script1);
   })
   .catch((error) => console.error("Error loading footer:", error));
